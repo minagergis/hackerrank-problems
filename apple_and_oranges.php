97610cd8024375c107277fb30554fb8a40871897
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-03-18 21:22:31 +0200

    Solving apples and oranges problem

---
+++ apple_and_oranges.php
@@ -0,0 +1,22 @@
+<?php
+
+function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges) {
+    
+    $applesCount = 0;
+    $orangeCount = 0;
+
+    foreach ($apples as $apple){
+        if ($a + $apple >= $s && $a + $apple <= $t){
+            $applesCount++;
+        }
+    }
+
+    foreach ($oranges as $orange){
+        if ($b + $orange >= $s && $b + $orange <= $t){
+            $orangeCount++;
+        }
+    }
+
+    echo $applesCount . '\n' .$orangeCount ;
+}
+
